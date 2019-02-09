633511d18860d29eaa4e2f34c1f07c371c0854b3
Joel Robichaud <robichaud.jo@gmail.com>
2019-02-09 12:48:04 -0500

    Drop cairo for now and added a basic audio test using portaudio.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use glfw::{Action, Context, Key};
+
 use nfd::Response;
+
+use portaudio as pa;
+use std::f64::consts::PI;
+use std::thread;
+
+// TODO: Display some kind of graphics through OpenGL and benchmark the perf
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
@@ -9,19 +16,6 @@ fn main() {
 
     window.set_key_polling(true);
     window.make_current();
-
-    // TODO: Create a cairo context from the native context
-    // and draw something on it!
-
-    // let (width, height) = window.get_size();
-    // let surface = cairo::QuartzSurface::create_for_cg_context(width, height);
-
-    // let cr = cairo::Context::new(&surface);
-    // cr.scale(120.0, 120.0);
-    // cr.set_line_width(0.1);
-    // cr.set_source_rgb(0.0, 0.0, 0.0);
-    // cr.rectangle(0.25, 0.25, 0.5, 0.5);
-    // cr.stroke();
 
     while !window.should_close() {
         glfw.poll_events();
@@ -37,16 +31,76 @@ fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
             window.set_should_close(true)
         },
         glfw::WindowEvent::Key(Key::O, _, Action::Press, _) => {
-            let result = nfd::open_file_dialog(None, None).unwrap_or_else(|e| {
-                panic!(e);
-            });
+            let result = nfd::dialog()
+                .filter("wav").open()
+                .expect("Failed to open file dialog.");
 
             match result {
                 Response::Okay(file_path) => println!("File path = {:?}", file_path),
-                Response::OkayMultiple(files) => println!("Files {:?}", files),
+                Response::OkayMultiple(_) => panic!("User should only be able to select a single file."),
                 Response::Cancel => println!("User canceled"),
             }
         },
+        glfw::WindowEvent::Key(Key::S, _, Action::Press, _) => {
+            thread::spawn(move || {
+                run_portaudio_test().expect("PortAudio Test: failed to run");
+            });
+        },
         _ => {}
     }
+}
+
+fn run_portaudio_test() -> Result<(), pa::Error> {
+    const CHANNELS: i32 = 2;
+    const NUM_SECONDS: i32 = 5;
+    const SAMPLE_RATE: f64 = 44_100.0;
+    const FRAMES_PER_BUFFER: u32 = 64;
+    const TABLE_SIZE: usize = 200;
+
+    println!("PortAudio Test: output sine wave. SR = {}, BufSize = {}", SAMPLE_RATE, FRAMES_PER_BUFFER);
+
+    // Initialise sinusoidal wavetable.
+    let mut sine = [0.0; TABLE_SIZE];
+    for i in 0..TABLE_SIZE {
+        sine[i] = (i as f64 / TABLE_SIZE as f64 * PI * 2.0).sin() as f32;
+    }
+    let mut left_phase = 0;
+    let mut right_phase = 0;
+
+    let pa = pa::PortAudio::new()?;
+
+    let mut settings = pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER)?;
+    // we won't output out of range samples so don't bother clipping them.
+    settings.flags = pa::stream_flags::CLIP_OFF;
+
+    // This routine will be called by the PortAudio engine when audio is needed. It may called at
+    // interrupt level on some machines so don't do anything that could mess up the system like
+    // dynamic resource allocation or IO.
+    let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
+        let mut idx = 0;
+        for _ in 0..frames {
+            buffer[idx]   = sine[left_phase];
+            buffer[idx+1] = sine[right_phase];
+            left_phase += 1;
+            if left_phase >= TABLE_SIZE { left_phase -= TABLE_SIZE; }
+            right_phase += 3;
+            if right_phase >= TABLE_SIZE { right_phase -= TABLE_SIZE; }
+            idx += 2;
+        }
+        pa::Continue
+    };
+
+    let mut stream = pa.open_non_blocking_stream(settings, callback)?;
+
+    stream.start()?;
+
+    println!("Play for {} seconds.", NUM_SECONDS);
+    pa.sleep(NUM_SECONDS * 1_000);
+
+    stream.stop()?;
+    stream.close()?;
+
+    println!("Test finished.");
+
+    Ok(())
 }
