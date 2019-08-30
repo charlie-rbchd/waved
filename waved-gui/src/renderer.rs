a93f2d471e8914273dc6249e533cbe3575eb6a26
Joel Robichaud <robichaud.jo@gmail.com>
2019-08-29 21:39:53 -0400

    Cleanup waveform drawing code and compute rms for positive and negative parts separately.

--- waved-gui/src/renderer.rs
+++ waved-gui/src/renderer.rs
@@ -31,44 +31,54 @@ fn draw_line(frame: &Frame, from: (f32, f32), to: (f32, f32)) {
 }
 
 fn draw_waveform(frame: &Frame, pos: (f32, f32), size: (f32, f32), samples: &Vec<f32>) {
-    let top = pos.1;
-    let left = pos.0;
     let width = size.0;
     let height = size.1;
     let half_height  = height * 0.5;
+    let top = pos.1;
+    let left = pos.0;
+
+    let draw_segment = |x: f32, weight: (f32, f32)| {
+        let line_height = (half_height * weight.0, half_height * weight.1);
+        let line_start_y = top + half_height - line_height.1;
+        let line_end_y = top + half_height + line_height.0;
+        draw_line(frame, (left + x, line_start_y), (left + x, line_end_y));
+    };
 
     draw_line(frame, (left, top + half_height), (left + width, top + half_height));
 
     let mut current_x = 0;
-    let mut current_rms = 0.0;
-    let mut num_samples_acc = 0;
+    let mut sum_of_squares = (0.0, 0.0);
+    let mut num_samples_acc = (0, 0);
 
-    // TODO: Compute RMS for negative and positive samples separately
-    // TODO: Cleanup this loop
     let num_samples = samples.len();
     for (i, s) in samples.iter().enumerate() {
         let x = (i as f32 / num_samples as f32 * width).round() as i32;
         if current_x == x {
-            current_rms += s * s;
-            num_samples_acc += 1;
+            if *s < 0.0 {
+                sum_of_squares.0 += s * s;
+                num_samples_acc.0 += 1;
+            } else if *s > 0.0 {
+                sum_of_squares.1 += s * s;
+                num_samples_acc.1 += 1;
+            } else {
+                num_samples_acc.0 += 1;
+                num_samples_acc.1 += 1;
+            }
         } else {
-            current_rms = (current_rms / num_samples_acc as f32).sqrt();
-            let line_height = height * current_rms;
-            let line_start_y = top + (height - line_height) * 0.5;
-            let line_end_y = line_start_y + line_height;
-            draw_line(frame, (left + x as f32, line_start_y), (left + x as f32, line_end_y));
-
+            draw_segment(x as f32, (
+                (sum_of_squares.0 / num_samples_acc.0 as f32).sqrt(),
+                (sum_of_squares.1 / num_samples_acc.1 as f32).sqrt()
+            ));
             current_x = x;
-            current_rms = 0.0;
-            num_samples_acc = 0;
+            sum_of_squares = (0.0, 0.0);
+            num_samples_acc = (0, 0);
         }
     }
 
-    current_rms = (current_rms / num_samples_acc as f32).sqrt();
-    let line_height = height * current_rms;
-    let line_start_y = top + (height - line_height) * 0.5;
-    let line_end_y = line_start_y + line_height;
-    draw_line(frame, (left + width, line_start_y), (left + width, line_end_y));
+    draw_segment(width, (
+        (sum_of_squares.0 / num_samples_acc.0 as f32).sqrt(),
+        (sum_of_squares.1 / num_samples_acc.1 as f32).sqrt()
+    ));
 }
 
 impl<'f> Renderer<'f> {
@@ -101,6 +111,7 @@ impl<'f> Renderer<'f> {
     pub fn render(&self, state: &State, viewport: (f32, f32), scale: f32) {
         self.context.frame(viewport, scale, |frame| {
             if let Some(file) = &state.current_file {
+                // TODO: Implement zoom and scroll
                 draw_waveform(&frame, (0.0, 0.0), viewport, &file.samples);
             }
         });
