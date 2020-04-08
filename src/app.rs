f0e13c26b32a3f2a562174a980f09e5532450481
Joel Robichaud <robichaud.jo@gmail.com>
2020-04-07 22:04:13 -0400

    Refactor audio code in waved-sndfile.

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,15 @@
 use glfw::{Action, Context, Glfw, Key, OpenGlProfileHint, SwapInterval, Window, WindowEvent, WindowHint, WindowMode, FAIL_ON_ERRORS};
 use libloading::Library;
-use cpal::{self, StreamData, UnknownTypeOutputBuffer, SampleFormat};
-use cpal::traits::{HostTrait, DeviceTrait, EventLoopTrait};
-use ringbuf::{self, RingBuffer};
 
 use std::cell::RefCell;
 use std::path::{Path, PathBuf};
 use std::sync::mpsc::Receiver;
 use std::thread_local;
-use std::thread;
-use std::time::Duration;
 
 use waved_core::state::{AudioFile, State};
 use waved_core::log::Logger;
-use waved_sndfile::samples_from_file;
+use waved_sndfile::io::samples_from_file;
+use waved_sndfile::playback::create_audio_thread;
 
 use crate::cli::CommandLineArgs;
 
@@ -83,73 +79,6 @@ fn clean_reloaded_dylib() {
 
 extern "C" fn refresh_callback(_window: *mut glfw::ffi::GLFWwindow) {
     app.with(|a| a.render_gui());
-}
-
-fn create_audio_thread(buffer_size: usize) {
-    let host = cpal::default_host();
-    let device = host.default_output_device().expect("No output device available.");
-
-    // TODO: We may need to support other sample formats depending on what
-    // the target systems provide.
-    let mut supported_formats_range = device.supported_output_formats()
-        .expect("Error while querying formats.");
-    let format = supported_formats_range.find(|f| f.data_type == SampleFormat::F32)
-        .expect("No supported device format.")
-        .with_max_sample_rate();
-    let channels = format.channels;
-    let sample_rate = format.sample_rate.0;
-
-    // TODO: Can we know the size of the underlying device buffer?
-    let ring = RingBuffer::<f32>::new(buffer_size);
-    let (mut producer, mut consumer) = ring.split();
-
-    // Spawn audio mixing thread.
-    thread::spawn(move || {
-        let mut t = 0.0;
-        let t_inc = 1.0 / sample_rate as f32;
-        let t_sleep = buffer_size as f32 / sample_rate as f32 * 0.5;
-
-        let f = 1000.0;
-        let w = 2.0 * std::f32::consts::PI * f;
-
-        loop {
-            while !producer.is_full() {
-                let s = (w * t as f32).sin();
-                producer.push(s).unwrap();
-                t += t_inc;
-            }
-            thread::sleep(Duration::from_secs_f32(t_sleep));
-        }
-    });
-
-    // Spawn audio device thread.
-    thread::spawn(move || {
-        let event_loop = host.event_loop();
-        let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
-        event_loop.play_stream(stream_id).expect("Failed to play_stream.");
-        event_loop.run(move |stream_id, stream_result| {
-            let stream_data = match stream_result {
-                Ok(data) => data,
-                Err(err) => {
-                    eprintln!("An error occurred on stream {:?}: {}", stream_id, err);
-                    return;
-                },
-            };
-
-            match stream_data {
-                StreamData::Output { buffer: UnknownTypeOutputBuffer::F32(mut buffer) } => {
-                    for (i, elem) in buffer.iter_mut().enumerate() {
-                        if i % channels as usize == 0 {
-                            *elem = consumer.pop().unwrap_or(0.0);
-                        } else {
-                            *elem = 0.0;
-                        }
-                    }
-                },
-                _ => unreachable!(),
-            }
-        });
-    });
 }
 
 #[allow(dead_code)]
--- waved-sndfile/src/lib.rs
+++ waved-sndfile/src/lib.rs
@@ -1,35 +1,3 @@
-use hound::WavReader;
-pub use hound::{Error, Sample, SampleFormat};
-
-use std::path::Path;
-
-mod algorithm;
-use algorithm::deinterleave;
-
-pub fn samples_from_file<P: AsRef<Path>>(filename: P) -> Result<(Vec<f32>, u16, u32), Error> {
-    let mut reader = WavReader::open(filename)?;
-    let spec = reader.spec();
-    let samples: Vec<_> = match spec.sample_format {
-        SampleFormat::Float => {
-            reader.samples::<f32>()
-                .map(|s| s.unwrap())
-                .collect()
-        },
-        SampleFormat::Int => {
-            // The range values assume the integers are encoded using two's complement
-            let neg_range = 2i32.pow((spec.bits_per_sample - 1).into());
-            let pos_range = neg_range - 1;
-            reader.samples::<i32>()
-                .map(|s| { 
-                    let s = s.unwrap();
-                    if s < 0 {
-                        (s as f64 / neg_range as f64) as f32
-                    } else {
-                        (s as f64 / pos_range as f64) as f32
-                    }
-                })
-                .collect()
-        },
-    };
-    Ok((deinterleave(&samples, spec.channels as usize), spec.channels, spec.sample_rate))
-}
+pub mod algorithm;
+pub mod io;
+pub mod playback;
