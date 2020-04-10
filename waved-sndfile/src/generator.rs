765e78e084a8cd61daaa815c19bd707c81df8dd8
Joel Robichaud <robichaud.jo@gmail.com>
2020-04-10 14:04:20 -0400

    Add square wave generator.

--- waved-sndfile/src/generator.rs
+++ waved-sndfile/src/generator.rs
@@ -10,3 +10,15 @@ pub fn sine(sample_rate: u32, frequency: f32) -> impl std::iter::Iterator<Item =
         Some(s)
     })
 }
+
+pub fn square(sample_rate: u32, frequency: f32) -> impl std::iter::Iterator<Item = f32> {
+    let mut t = 0.0_f64;
+    let t_inc = 1.0_f64 / sample_rate as f64;
+
+    std::iter::from_fn(move || {
+        let ft = frequency * t as f32;
+        let s = 2.0_f32 * (2.0_f32 * ft.floor() - (2.0_f32 * ft).floor()) - 1.0_f32;
+        t += t_inc;
+        Some(s)
+    })
+}
